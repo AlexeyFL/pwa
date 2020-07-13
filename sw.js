92c88212eabfd744647cd500db20087ef51ab956
AlexeyFL <alexeyfl563@gmail.com>
2020-07-13 22:48:13 +0300

    Cache versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
 const assets = [
   '/',
   'app.js',
@@ -23,7 +23,15 @@ self.addEventListener('install', (evt) => {
 
 // activate service wor
 self.addEventListener('activate', (evt) => {
-  // console.log('service worker has been activated');
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  );
 });
 
 // fetch
@@ -31,8 +39,8 @@ self.addEventListener('fetch', (evt) => {
   // console.log('fetch event', evt);
 
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request)
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
     })
-  )
+  );
 });
