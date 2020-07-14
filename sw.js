ca1a2a9072d42242f8b3cc470e37769c9603d612
AlexeyFL <alexeyfl563@gmail.com>
2020-07-14 19:33:32 +0300

    add fallback page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const staticCacheName = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   'app.js',
@@ -10,6 +10,7 @@ const assets = [
   '/images/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  '/pages/fallback.html',
 ];
 
 // install service worker
@@ -28,7 +29,7 @@ self.addEventListener('activate', (evt) => {
     caches.keys().then((keys) => {
       return Promise.all(
         keys
-          .filter((key) => key !== staticCacheName)
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
           .map((key) => caches.delete(key))
       );
     })
@@ -40,16 +41,19 @@ self.addEventListener('fetch', (evt) => {
   // console.log('fetch event', evt);
 
   evt.respondWith(
-    caches.match(evt.request).then((cacheRes) => {
-      return (
-        cacheRes ||
-        fetch(evt.request).then((fetchRes) => {
-          return caches.open(dynamicCache).then((cache) => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes; 
-          });
-        })
-      );
-    })
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      }) 
+      .catch(() => caches.match('/pages/fallback.html'))
   );
 });
